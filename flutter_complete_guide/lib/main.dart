1aba46bbf5a963686688fe9b26dd23389e4577b0
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-07-10 11:35:49 +0900

    add: リストの追加とcolumnの追加

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -9,6 +9,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // homeが名前付き変数として定義されている
-    return MaterialApp(home: Text('Hello!'));
+    // return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'What\'s your favorite color? ',
+      'What\'s your favorite animal? ',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
   }
 }
