d6c183fe6f5af8e29d1920c1deb173973f35d2d4
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-07-10 10:14:19 +0900

    add: 名前付き変数

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    // homeが名前付き変数として定義されている
     return MaterialApp(home: Text('Hello!'));
   }
 }
