4c109bc72dead6d0fbf3a1610d949f988fca5114
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-07-16 08:04:13 +0900

    add: テキストが切り替わらない状態

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,14 +7,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer Chosen');
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     // homeが名前付き変数として定義されている
-    // return MaterialApp(home: Text('Hello!'));
+    // return MaterialApp(home: Text('Hello!'))
+
     var questions = [
       'What\'s your favorite color? ',
       'What\'s your favorite animal? ',
@@ -26,7 +30,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
             RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen!')),
             RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
