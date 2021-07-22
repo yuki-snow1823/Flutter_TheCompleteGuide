6d6f3bdaa487f2968bc082674a3ce790d3a1e267
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-07-22 14:24:21 +0900

    be final

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -41,8 +43,8 @@ class _MyAppState extends State < MyApp > {
           title: Text('My First App'),
         ),
         body: Column(
-          children: < Widget > [
-            Text(questions[_questionIndex]),
+          children: [
+            Question(questions[_questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
             RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen!')),
             RaisedButton(child: Text('Answer 3'), onPressed: _answerQuestion),
